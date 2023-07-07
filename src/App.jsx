bcc14a7521598e5735e868c3a8b9739526a9de12
Wieser Matthias <extern.wieser_matthias@allianz.de>
2023-07-07 08:55:53 +0200

    feat: m0002 - readd App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import MainPage from "./component/MainPage";
+
+const App = () => {
+	return (
+		<>
+			<header>Implement a header component here!</header>
+			<main>
+				<MainPage />
+			</main>
+			<footer>Implement a footer component here!</footer>
+		</>
+	);
+};
+
+export default App;
